b3f7fa48cd46c561d7ead5a3946d5f6afca0ba1e
Alexander Potts <alex.potts@edg.com.au>
2023-01-15 01:06:21 +1100

    Add draft status enum and meta class

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,9 +1,36 @@
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
+    # Another pattern for establishing enumeration-style choices
+    # https://docs.djangoproject.com/en/4.1/ref/models/fields/#enumeration-types
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+
+    # Django pattern: nested class for defining metadata
+    # https://docs.djangoproject.com/en/4.1/topics/db/models/#meta-options
+    # https://docs.djangoproject.com/en/4.1/ref/models/options/
+    class Meta:
+        # Blogs typically order by latest published...
+        ordering = ['-publish']
+
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
 
     def __str__(self):
         return self.title
