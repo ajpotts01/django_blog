b321737e236934bf728b5dc64fa32c8b0735302e
Alexander Potts <alex.potts@edg.com.au>
2023-01-15 22:50:51 +1100

    First run of views for post lists/details

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,28 +2,25 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
     # Another pattern for establishing enumeration-style choices
     # https://docs.djangoproject.com/en/4.1/ref/models/fields/#enumeration-types
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
+        to=User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(
-        max_length=2,
-        choices=Status.choices,
-        default=Status.DRAFT
+        max_length=2, choices=Status.choices, default=Status.DRAFT
     )
 
     # Django pattern: nested class for defining metadata
@@ -31,12 +28,9 @@ class Post(models.Model):
     # https://docs.djangoproject.com/en/4.1/ref/models/options/
     class Meta:
         # Blogs typically order by latest published...
-        ordering = ['-publish']
+        ordering = ["-publish"]
 
-        indexes = [
-            models.Index(fields=['-publish'])
-        ]
-
+        indexes = [models.Index(fields=["-publish"])]
 
     def __str__(self):
         return self.title
